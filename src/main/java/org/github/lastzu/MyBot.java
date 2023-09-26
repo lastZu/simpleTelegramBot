ac9ecdf9f65e0a81911369719cea33d99fc08815
lastZu <cdrom2015@ya.ru>
2023-09-26 18:19:26 +0300

    add echo text answer

--- src/main/java/org/github/lastzu/MyBot.java
+++ src/main/java/org/github/lastzu/MyBot.java
@@ -1,18 +1,43 @@
 package org.github.lastzu;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
 public class MyBot extends TelegramLongPollingBot {
+    private String name;
+
+    public MyBot(String name) {
+        this.name = name;
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
+        if (!update.hasMessage()) return;
 
+        Message message = update.getMessage();
+        if (!message.hasText()) return;
+
+        String text = message.getText();
+        long chatId = message.getChatId();
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(text);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String getBotUsername() {
-        return "";
+        return name;
     }
 }
