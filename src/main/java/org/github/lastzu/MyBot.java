fd1255794e5d8f83136693f285303450859acced
lastZu <cdrom2015@ya.ru>
2023-09-29 16:31:15 +0300

    check MyBot

--- src/main/java/org/github/lastzu/MyBot.java
+++ src/main/java/org/github/lastzu/MyBot.java
@@ -9,21 +9,27 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-
-public class MyBot extends TelegramLongPollingBot {
-    final Logger logger = LoggerFactory.getLogger(MyBot.class);
+/**
+ * MyBot is telegram long polling bot. For create it needs
+ * - Name registered in BotFather
+ * - token received in BotFather
+ */
+final class MyBot extends TelegramLongPollingBot {
+    private final Logger logger = LoggerFactory.getLogger(MyBot.class);
     private final String name;
 
-    public MyBot(String name, String token) {
+    public MyBot(final String name, final String token) {
         super(token);
         this.name = name;
     }
 
     @Override
-    public void onUpdateReceived(Update update) {
+    public void onUpdateReceived(final Update update) {
         logger.debug("Receive update");
 
-        if (!update.hasMessage()) return;
+        if (!update.hasMessage()) {
+            return;
+        }
         logger.info("Receive update");
 
         Message message = update.getMessage();
---
+++ src/main/java/org/github/lastzu/package-info.java
@@ -0,0 +1,4 @@
+/**
+ *
+ */
+package org.github.lastzu;
